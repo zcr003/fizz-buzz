21fe1dc1a289c99e36a1e613635eeb11f575d0c4
zcr003 <zramsey1@cnm.edu>
2021-09-29 12:05:14 -0600

    Initial commit

---
+++ src/main/java/edu/cnm/deepdive/FizzBuzz.java
@@ -0,0 +1,23 @@
+package edu.cnm.deepdive;
+
+public class FizzBuzz {
+
+  public String getFizzBuzz(int value) {
+    String str;
+    /*Remember, everything else goes here, similar to Flowgarithm Chart!! */
+    if (value % 3 == 0) {
+      if (value % 5 == 0) {
+        str = "FizzBuzz";
+      } else {
+        str = "Fizz";
+      }
+    } else {
+      if (value % 5 == 0) {
+        str = "Buzz";
+      } else {
+        str = String.valueOf(value);
+      }
+    }
+    return str;
+  }
+}
