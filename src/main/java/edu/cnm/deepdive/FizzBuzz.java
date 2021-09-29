395591de01c152fb772c77b7f1189364f38e75b4
zcr003 <zramsey1@cnm.edu>
2021-09-29 15:19:46 -0600

    Added chain of Tests for fizz, buzz, & fizzbuzz

--- src/main/java/edu/cnm/deepdive/FizzBuzz.java
+++ src/main/java/edu/cnm/deepdive/FizzBuzz.java
@@ -2,17 +2,22 @@ package edu.cnm.deepdive;
 
 public class FizzBuzz {
 
+
+  static final String FIZZ_BUZZ_VALUE = "FIZZ_BUZZ";
+  static final String FIZZ_VALUE = "Fizz";
+  static final String BUZZ_VALUE = "Buzz";
+
   public String getFizzBuzz(int value) {
     String str;
     /*Remember, everything else goes here, similar to Flowgarithm Chart!! */
     if (value % 3 == 0) {
       if (value % 5 == 0) {
-        str = "FizzBuzz";
+        str = FIZZ_BUZZ_VALUE;
       } else {
-        str = "Fizz";
+        str = FIZZ_VALUE;
       }
     } else if (value % 5 == 0) {
-      str = "Buzz";
+      str = BUZZ_VALUE;
     } else {
       str = String.valueOf(value);
     }
---
+++ src/test/java/edu/cnm/deepdive/FizzBuzzTest.java
@@ -0,0 +1,37 @@
+package edu.cnm.deepdive;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+
+class FizzBuzzTest {
+
+  static final int[] fizzValues = {21, 999, 3, 9};
+  static final int[] buzzValues = {10, 95, 65, 500};
+  static final int[] fizzBuzzValues = {15, 90, 0, 60, 135, 1500};
+  static final int[] neitherValues = {17, 2, 98, 7, 97};
+
+
+  @Test
+  @DisplayName("Test Buzz values")
+  void getFizzBuzz_buzz() {
+    FizzBuzz fb = new FizzBuzz();
+    for (int value : buzzValues) {
+      assertEquals(FizzBuzz.BUZZ_VALUE, fb.getFizzBuzz(value));
+    }
+  }
+
+  @Test
+  @DisplayName("Test Fizz_Buzz values")
+  void getFizzBuzz_fizzBuzz() {
+    FizzBuzz fb = new FizzBuzz();
+    for (int value : fizzBuzzValues) {
+      assertEquals(FizzBuzz.FIZZ_BUZZ_VALUE, fb.getFizzBuzz(value));
+    }
+  }
+
+
+
+
+}
